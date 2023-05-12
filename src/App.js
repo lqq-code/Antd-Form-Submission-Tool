8a946f418eb80c46dd29aa6cf2a127eb52e8d748
choco <choco.luo@playerauctions.com>
2023-05-12 16:58:50 +0800

    初始化

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Monitor from './pages/ThreeHouse/ThreeMap';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+            <Monitor/>
+      </div>
+    );
+  }
+}
+
+export default App;
